cd5c58e52624f842de004b264bf0360282a6d073
Emrys025 <c3gu@ucsd.edu>
2025-05-01 20:36:19 -0400

    try changing PL target density

--- src/config.tcl
+++ src/config.tcl
@@ -30,13 +30,13 @@ set ::env(BOTTOM_MARGIN_MULT) 2
 
 # absolute die size
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 1000 1000"
+set ::env(DIE_AREA) "0 0 1280 1280"
 
 ## THIS IS THE ONLY LINE YOU CAN EDIT
 set ::env(FP_CORE_UTIL) 45
 
-set ::env(PL_BASIC_PLACEMENT) 0
-set ::env(PL_TARGET_DENSITY) 0.60
+#set ::env(PL_BASIC_PLACEMENT) 0
+#set ::env(PL_TARGET_DENSITY) 0.60
 
 ## set ::env(PL_BASIC_PLACEMENT) {1}
 
