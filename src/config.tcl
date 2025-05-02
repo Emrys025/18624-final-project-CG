a8188f4b5ad44047a43ac93ccf93c087140a8338
Emrys025 <c3gu@ucsd.edu>
2025-05-01 21:24:51 -0400

    try changing placement density

--- src/config.tcl
+++ src/config.tcl
@@ -36,7 +36,7 @@ set ::env(DIE_AREA) "0 0 1600 1600"
 set ::env(FP_CORE_UTIL) 45
 
 #set ::env(PL_BASIC_PLACEMENT) 0
-#set ::env(PL_TARGET_DENSITY) 0.60
+set ::env(PL_TARGET_DENSITY) 0.40
 #set ::env(DIODE_INSERTION_STRATEGY) 3
 
 ## set ::env(PL_BASIC_PLACEMENT) {1}
