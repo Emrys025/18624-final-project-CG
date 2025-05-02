207909240e6ff11e8a605b0334990a7ff39489ca
Emrys025 <c3gu@ucsd.edu>
2025-05-01 20:18:25 -0400

    try changing PL target density

--- src/config.tcl
+++ src/config.tcl
@@ -35,6 +35,9 @@ set ::env(DIE_AREA) "0 0 1000 1000"
 ## THIS IS THE ONLY LINE YOU CAN EDIT
 set ::env(FP_CORE_UTIL) 45
 
+set ::env(PL_BASIC_PLACEMENT) 0
+set ::env(PL_TARGET_DENSITY) 0.40
+
 ## set ::env(PL_BASIC_PLACEMENT) {1}
 
 set ::env(FP_IO_HLENGTH) 2
