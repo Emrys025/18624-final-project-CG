99a21fc7e312a156f41e56fed517cc78bdf213d3
Emrys025 <c3gu@ucsd.edu>
2025-05-04 12:30:42 -0400

    normal setting

--- src/config.tcl
+++ src/config.tcl
@@ -30,14 +30,12 @@ set ::env(BOTTOM_MARGIN_MULT) 2
 
 # absolute die size
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 1300 1300"
+set ::env(DIE_AREA) "0 0 1800 1800"
 
 ## THIS IS THE ONLY LINE YOU CAN EDIT
 set ::env(FP_CORE_UTIL) 45
 
-#set ::env(PL_BASIC_PLACEMENT) 0
-#set ::env(PL_TARGET_DENSITY) 0.35
-#set ::env(DIODE_INSERTION_STRATEGY) 3
+set ::env(PL_TARGET_DENSITY) 0.35
 
 ## set ::env(PL_BASIC_PLACEMENT) {1}
 
